890b62960b1ec93ffde31f52659133f7271703ce
Farid Rafati <36322961+faridrafati@users.noreply.github.com>
2024-12-14 22:29:30 +0330

    Update MyContract.sol

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract MyContract is Initializable {
+contract MyContract is Initializable{
     uint256 public myValue;
 
-    function initialize(uint256 _value) public initializer {
-        myValue = _value;
+    function Initialize(uint256 _myValue) public initializer {
+        myValue = _myValue;
     }
 }
